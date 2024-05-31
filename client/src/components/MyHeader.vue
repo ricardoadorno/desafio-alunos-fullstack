c4d97b52283f9a7f937787ce3d3a33ad5ea95866
Ricardo Adorno <ricardo.castrorc1998@gmail.com>
2024-05-31 14:45:49 -0300

    refactor(MyHeader): create slots for composability

--- client/src/components/MyHeader.vue
+++ client/src/components/MyHeader.vue
@@ -1,6 +1,4 @@
-<script setup lang="ts">
-import StudentForm from './StudentForm.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="card w-full bg-neutral shadow-xl">
@@ -16,31 +14,9 @@ import StudentForm from './StudentForm.vue'
       <div
         class="flex items-center gap-4 mt-4 flex-col md:flex-row md:justify-between md:items-center"
       >
-        <div class="flex flex-col gap-4 w-full md:flex-row">
-          <input type="text" placeholder="Name" class="input input-bordered" />
-          <input type="text" placeholder="CPF" class="input input-bordered" />
-          <input type="text" placeholder="Email" class="input input-bordered" />
-          <button class="btn btn-primary">Search</button>
-        </div>
+        <slot name="form-search"></slot>
 
-        <button
-          class="btn btn-success w-full md:w-auto md:ml-4 mt-4 md:mt-0"
-          onclick="add_modal.showModal()"
-        >
-          Add
-        </button>
-        <dialog id="add_modal" class="modal">
-          <div class="modal-box">
-            <h3 class="font-bold text-lg">Add a new student</h3>
-            <StudentForm />
-            <div class="modal-action">
-              <form method="dialog">
-                <button class="btn btn-error btn-outline mr-4">Cancel</button>
-                <button class="btn btn-success">Add</button>
-              </form>
-            </div>
-          </div>
-        </dialog>
+        <slot name="form-add"></slot>
       </div>
     </div>
   </div>
