adde5744eea3d417c8093031680c9c71c99b43e7
Ricardo Adorno <ricardo.castrorc1998@gmail.com>
2024-06-02 16:41:24 -0300

    feature(MyTypography): create atomic component

---
+++ client/src/components/atoms/MyTypography.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    variant: keyof typeof variantMap
+    tag?: keyof HTMLElementTagNameMap
+  }>(),
+  {
+    variant: 'body'
+  }
+)
+
+const variantMap = {
+  heading1: 'text-3xl font-bold underline',
+  heading2: 'font-bold text-lg',
+  heading3: 'text-2xl font-bold',
+  body: 'text-md',
+  caption: 'text-sm text-neutral-content italic'
+}
+
+const defaultTagMap = {
+  heading1: 'h1',
+  heading2: 'h2',
+  heading3: 'h3',
+  body: 'p',
+  caption: 'span'
+}
+</script>
+
+<template>
+  <component :is="tag ? tag : defaultTagMap[variant]" :class="variantMap[variant]">
+    <slot />
+  </component>
+</template>
