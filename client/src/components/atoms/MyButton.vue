b14e00f01521847da4863d2c5cc23fee774fc828
Ricardo Adorno <ricardo.castrorc1998@gmail.com>
2024-06-02 16:41:08 -0300

    feature(MyButton): create atomic component

---
+++ client/src/components/atoms/MyButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    color: 'primary' | 'secondary' | 'error' | 'success' | 'info'
+    variant: 'solid' | 'outline'
+  }>(),
+  {
+    color: 'primary',
+    variant: 'solid'
+  }
+)
+</script>
+
+<template>
+  <button :class="['btn', `btn-${variant}`, `btn-${color}`]">
+    <slot />
+  </button>
+</template>
